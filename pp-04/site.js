6235a32f88810805b6953ad0e899fdfb90f2dfbc
Matthew Hofmann <mhofmann@hawk.iit.edu>
2018-11-12 01:38:59 -0600

    Configure button

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,9 +1,8 @@
-document.addEventListener('DOMContentLoaded', function(event){
-document.querySelector('html').className = 'js';
-var newbell = new Audio('media/doorbell.mp3')
-})
+  var newbell = new Audio('media/doorbell.mp3');
 
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+  function ringDoorbell() {
+    newbell.play();
+  }
+
+  document.querySelector('#doorbell').innerHTML = "<button id='doorbutton'>Click to Ring the Doorbell</button>";
+  document.querySelector

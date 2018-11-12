059acbf048bd2d6ba2a72d4f4789d3f4f1d2aac3
Matthew Hofmann <mhofmann@hawk.iit.edu>
2018-11-12 00:00:17 -0600

    Add skeletal js

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,3 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(event){
+document.querySelector('html').className = 'js';
+var newbell = new Audio('media/doorbell.mp3')
+})
+
 function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
